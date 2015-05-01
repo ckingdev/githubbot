1b63dc520c7ffff236dd52e5b19086ab5a15ab99
M. Cameron Palone <mcameronpalone@gmail.com>
2015-04-30 21:43:44 -0600

    Added link to message.

--- github.go
+++ github.go
@@ -11,16 +11,18 @@ func (s *Session) hookServer(port int, secret string) {
 	server := hookserve.NewServer()
 	server.Port = port
 	server.Secret = secret
+	s.logger.Debug("Starting webhook server...")
 	server.GoListenAndServe()
-
+	s.logger.Debug("...started.")
 	for {
 		select {
 		case event := <-server.Events:
+			URL := fmt.Sprintf("github.com/%s/%s/commits/%s", event.Owner, event.Repo, event.Commit)
 			s.logger.Debug("Received webhook event of type: %s", event.Type)
 			if event.Type != "push" {
 				continue
 			}
-			s.sendMessage(fmt.Sprintf("[%s | %s] Commit pushed. %s", event.Repo, event.Branch, event.Commit), "")
+			s.sendMessage(fmt.Sprintf("[%s | %s] Commit pushed. %s", event.Repo, event.Branch, URL), "")
 		case <-time.After(time.Duration(30) * time.Second):
 			s.logger.Debug("Timed out without an event.")
 		}
