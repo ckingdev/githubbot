d0d3cd330973f81878aff349716a9ad49ec52c47
M. Cameron Palone <mcameronpalone@gmail.com>
2015-05-01 03:27:37 -0600

    Added support for issues events.

--- github.go
+++ github.go
@@ -30,15 +30,39 @@ func (s *Session) hookServer(port int, secret string) {
 				payload.HeadCommit.URL,
 			)
 			s.sendMessage(msg, "")
-		case gohook.CommitCommentType:
+		case gohook.CommitCommentEventType:
 			payload, ok := et.Event.(*gohook.CommitCommentEvent)
 			if !ok {
-				panic("Malformed *CommitCommentEvent")
+				panic("Malformed *CommitCommentEvent.")
 			}
 			msg := fmt.Sprintf("[ %s ] Comment on commit: %s (%s)",
 				payload.Repository.Name,
 				payload.Comment.Body,
 				payload.Comment.HTMLURL,
+			)
+			s.sendMessage(msg, "")
+		case gohook.IssueCommentEventType:
+			payload, ok := et.Event.(*gohook.IssueCommentEvent)
+			if !ok {
+				panic("Malformed *CommitCommentEvent.")
+			}
+			msg := fmt.Sprintf("[ %s ] Comment on issue '%s': %s (%s)",
+				payload.Repository.Name,
+				payload.Issue.Title,
+				payload.Comment.Body,
+				payload.Comment.HTMLURL,
+			)
+			s.sendMessage(msg, "")
+		case gohook.IssuesEventType:
+			payload, ok := et.Event.(*gohook.IssuesEvent)
+			if !ok {
+				panic("Malformed *IssuesEvent.")
+			}
+			msg := fmt.Sprintf("[ %s ] Issue '%s' was %s. (%s)",
+				payload.Repository.Name,
+				payload.Issue.Title,
+				payload.Action,
+				payload.Issue.HTMLURL,
 			)
 			s.sendMessage(msg, "")
 		}
